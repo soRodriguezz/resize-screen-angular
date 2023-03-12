51d08fd35bb5d3714028412f83b0e017a80cde26
soRodriguezz <s.rodriguezzapata01@gmail.com>
2023-03-12 19:51:00 -0300

    (feat) Se crea redimencionamiento de web

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'resize-screen-angular';
+
+  screenWidth: number = 0;
+  screenHeight: number = 0;
+
+  ngOnInit() {
+    this.onResize();
+  }
+
+  calculateStyles() {
+    let styles: any = {};
+    if (this.screenWidth < 768) {
+      styles['background-color'] = 'red';
+    } else {
+      styles['background-color'] = 'green';
+    }
+    return styles;
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    this.screenWidth = window.innerWidth;
+    this.screenHeight = window.innerHeight;
+  }
+
+  ngOnDestroy() {
+    window.removeEventListener('resize', this.onResize);
+  }
 }
